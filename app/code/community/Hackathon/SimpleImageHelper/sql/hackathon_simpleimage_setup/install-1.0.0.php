db3363708b2627fddbce572b2f762640c45dfe82
Daniel Niedergesäß <me@danielniedergesaess.de>
2014-02-01 00:59:01 +0100

    changed setup model and removed unused entries from create attribute

--- app/code/community/Hackathon/SimpleImageHelper/sql/hackathon_simpleimage_setup/install-1.0.0.php
+++ app/code/community/Hackathon/SimpleImageHelper/sql/hackathon_simpleimage_setup/install-1.0.0.php
@@ -16,24 +16,14 @@ $installer->startSetup();
 $this->addAttribute(Mage_Catalog_Model_Product::ENTITY, 'simpleimage_assets', array(
     'group' => 'General',
     'type' => 'text',
-    'backend' => '',
-    'frontend' => '',
     'label' => 'Simple Image Assets',
     'input' => 'text',
-    'source' => '',
     'global' => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_GLOBAL,
     'visible' => true,
     'required' => false,
     'user_defined' => true,
-    'default' => '',
-    'searchable' => false,
-    'filterable' => true,
-    'comparable' => false,
     'visible_on_front' => true,
-    'visible_in_advanced_search' => true,
     'used_in_product_listing' => true,
-    'unique' => false,
-    'apply_to' => 'simple',
 ));
 
 $installer->endSetup();
