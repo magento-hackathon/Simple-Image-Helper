86937374a7efdb9f8f9ea4e613d64f162f383e6f
James Cowie <james@space48.com>
2014-01-31 21:56:42 +0000

    Fixed observer to fire correctly, Loop through images ready to process them

--- app/code/community/Hackathon/SimpleImageHelper/Model/Observer.php
+++ app/code/community/Hackathon/SimpleImageHelper/Model/Observer.php
@@ -6,8 +6,14 @@ class Hackathon_SimpleImageHelper_Model_Observer
 {
     public function getImages(Varien_Event_Observer $observer)
     {
-        $product = $observer->getData('product');
+        $product = $observer->getData('product');        
+        $images  = $product->getData('media_gallery');
         
-        
+        foreach ($images as $image) {
+            foreach ($image as $img) {
+                // make call here to helper to resize image based on file $img['file];
+            }
+        }
     }
+    
 }
