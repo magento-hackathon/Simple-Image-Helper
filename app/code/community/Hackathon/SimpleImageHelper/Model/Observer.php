eca3be0dcb1a3a4e3b6cdba23e157f55b727e135
James Cowie <james@space48.com>
2014-01-31 22:03:31 +0000

    Refactor method used to get media gallery images

--- app/code/community/Hackathon/SimpleImageHelper/Model/Observer.php
+++ app/code/community/Hackathon/SimpleImageHelper/Model/Observer.php
@@ -7,8 +7,8 @@ class Hackathon_SimpleImageHelper_Model_Observer
     public function getImages(Varien_Event_Observer $observer)
     {
         $product = $observer->getData('product');        
-        $images  = $product->getData('media_gallery');
-        
+        $images  = $product->getMediaGallery();
+
         foreach ($images as $image) {
             foreach ($image as $img) {
                 // make call here to helper to resize image based on file $img['file];
