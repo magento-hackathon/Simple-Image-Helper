261abc854b97ac2c90a3214ed3bb2702b97d8a0f
James Cowie <james@space48.com>
2014-01-31 21:36:56 +0000

    Added in initial observer for after_product_save

--- app/code/community/Hackathon/SimpleImageHelper/Model/Observer.php
+++ app/code/community/Hackathon/SimpleImageHelper/Model/Observer.php
@@ -4,5 +4,10 @@
  */
 class Hackathon_SimpleImageHelper_Model_Observer
 {
-    
+    public function getImages(Varien_Event_Observer $observer)
+    {
+        $product = $observer->getData('product');
+        
+        
+    }
 }
