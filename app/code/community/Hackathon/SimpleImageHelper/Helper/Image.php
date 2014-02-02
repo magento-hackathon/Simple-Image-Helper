561cc6ede60570869ec03a0d72a8a9f02ec513b3
beeplogic <beeplogic@magenation.com>
2014-02-01 16:01:34 -0800

    update helper to be used for frontend add in rewrite

--- app/code/community/Hackathon/SimpleImageHelper/Helper/Image.php
+++ app/code/community/Hackathon/SimpleImageHelper/Helper/Image.php
@@ -6,27 +6,31 @@
  * @category Hackathon
  * @package  Hackathon_SimpleImageHelper
  * @author   Florinel Chis <florinel.chis@gmail.com>
+ * @author   Rolando Granadino <beeplogic@gmail.com>
  * @license  Open Software License (OSL 3.0)
  * @link     http://opensource.org/licenses/osl-3.0.php
  */
-
-
-class Hackathon_SimpleImageHelper_Helper_Image extends Mage_Core_Helper_Abstract
+class Hackathon_SimpleImageHelper_Helper_Image extends Mage_Catalog_Helper_Image
 {
 
     /**
-     * Current Product
-     *
-     * @var Mage_Catalog_Model_Product
+     * base data helper
+     * @var Hackathon_SimpleImageHelper_Helper_Data
      */
-    protected $_product;
+    protected $_dataHelper;
+    /**
+     * frontend url path
+     * @var str
+     */
+    protected $_frontendPath;
 
     /**
-     * Image attribute name: small_image, thumbnail
-     *
-     * @var string
+     * setup core data helper
      */
-    protected $_attributeName;
+    public function __construct()
+    {
+        $this->_dataHelper = Mage::helper('hackathon_simpleimage');
+    }
 
     /**
      * Initialize Helper to work with Image
@@ -36,35 +40,66 @@ class Hackathon_SimpleImageHelper_Helper_Image extends Mage_Core_Helper_Abstract
      * @param mixed $imageFile
      * @return Mage_Catalog_Helper_Image
      */
-    public function init(Mage_Catalog_Model_Product $product, $attributeName, $imageFile=null)
+    public function init(Mage_Catalog_Model_Product $product, $attributeName, $imageFile = null)
     {
-        $this->_product = $product;
-        $this->_attributeName = $attributeName;
-
-        return $this;
-    }
-
-    /**
-     * Do Nothing
-     *
-     * @see Mage_Catalog_Model_Product_Image
-     * @param int $width
-     * @param int $height
-     * @return Hackathon_SimpleImageHelper_Helper_Data
-     */
-    public function resize($width, $height = null)
-    {
+        $this->_frontendPath = null;
+        if ((Mage::app()->getStore()->isAdmin())) {
+            parent::init($product, $attributeName, $imageFile);
+        } else {
+            $assetPaths = $this->_initProductAssets($product);
+            if ($assetPaths && isset($assetPaths['site_images'][$attributeName])) {
+                $this->_frontendPath =  Mage::getBaseUrl('media').$assetPaths['site_images'][$attributeName]['path'];
+            }
+            if (!$this->_frontendPath) {//fall back and use placeholder
+                parent::init($product, $attributeName, $imageFile);
+            }
+        }
         return $this;
     }
 
     /**
      * Return Image URL
-     *
+     * overload to string method to send frontend path
+     * if we've generated one
+     * @see Mage_Catalog_Helper_Image::__toString
      * @return string
      */
     public function __toString()
     {
-        $product_id = $this->_product->getId();
-        return "/".$this->_attributeName.'/'.$product_id;
+        if ($this->_frontendPath) {
+            return $this->_frontendPath;
+        }
+        return parent::__toString();
+    }
+
+    /**
+     * unseralize and cache product image asset paths
+     * @param Mage_Catalog_Model_Product $product
+     * @return array
+     */
+    protected function _initProductAssets(Mage_Catalog_Model_Product $product)
+    {
+        if (!$product->hasData('frontend_simpleimage_assets')) {
+            $assetPaths = $this->_dataHelper->jsonDecode($product->getData($this->_dataHelper->getAttributeCode()));
+            if (is_array($assetPaths)) {
+                $paths = $assetPaths;
+            } else {
+                $paths = false;
+            }
+            $product->setData('frontend_simpleimage_assets', $paths);
+        }
+        return $product->getData('frontend_simpleimage_assets');
+    }
+
+    /**
+     * resize wrapper method 
+     * @see Mage_Catalog_Helper_Image::resize
+     */
+    public function resize($width, $height)
+    {
+        if (!$this->_frontendPath) {
+            return parent::resize($width);
+        }
+        return $this;
     }
 }
